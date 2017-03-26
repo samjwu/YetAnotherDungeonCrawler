d6d05a60324804c80a01aabb8f81d37d30aa1c0f
ishchow <ishaatchowdhury@gmail.com>
2017-03-26 01:09:24 -0600

    Fixed room placement within dungeon generation algorithm
    With this new version of the algorithm, the placed rooms are spaced out
    such that they don't collide and have enough space to fit a hallway
    between them. In addition, the old code had some off by one errors so
    the visualizer lines would overlap each other which was bad. Now all the
    dividing lines do not overlap and the generated rooms fall within the
    boundaries provided by these lines, which proves that the algorithm is
    working as intended. Note that some of these improvements were obtained
    by being more precise in the definition of the Room and Dungeon objects
    such that the invariant properties of an instance of each object make it
    so that room collision can never happen.
    
    Now what's next is to create hallways between each of the room such that
    the entire dungeon is connected.

--- level.py
+++ level.py
@@ -56,9 +56,12 @@ class Room(pygame.sprite.Group):
     """ Class used to represent a room
 
         A room is defined as a collection of tiles consisting of an interior
-        with positive area surrounded by walls. Therefore, the minimum room
-        width and height is 3. This leads to an area of 9 with 1 tile for the
-        interior and 8 for the border.
+        with positive area surrounded by walls. Therefore, the absolute minimum
+        room width and height is 3. This leads to an area of 9 with 1 tile for
+        the interior and 8 for the border. However, the minimum room dimensions
+        can be raised to increase playability of the game as the absolute
+        minimum dimensions are not really playable. Also, the maximum room
+        dimensions can be set for no other reason than playability.
 
         A room is located in space based upon the (x, y) position of its
         top-left tile.
@@ -100,17 +103,18 @@ class Room(pygame.sprite.Group):
                 self.add(Tile(FLOOR, col, row))
 
     # Class variables and methods
-    MIN_WIDTH = 5
-    MAX_WIDTH = 8
+    MIN_WIDTH = 4
+    MAX_WIDTH = 10
 
-    MIN_HEIGHT = 5
-    MAX_HEIGHT = 8
+    MIN_HEIGHT = 4
+    MAX_HEIGHT = 10
     @classmethod
     def generate_room(cls, region_x, region_y, region_width, region_height):
         """ Generate room enclosed within a specified region
 
-            Simply creates a room within a specified region. Does not check if
-            room collides with exisiting rooms.
+            Simply creates a room within a specified region (aka dungeon as
+            defiend in the dungeon class). Does not check if room collides with
+            exisiting rooms.
 
             Arguments:
                 region_x (int): x position of topleft corner of enclosing region
@@ -121,18 +125,29 @@ class Room(pygame.sprite.Group):
             Returns:
                 room (Room()): an instance of the Room class
         """
-        if region_width < cls.MIN_WIDTH or region_height < cls.MIN_HEIGHT:
-                return None
-        elif region_width == cls.MIN_WIDTH and region_height == cls.MIN_HEIGHT:
-            room = cls(region_x, region_y, region_width, region_height)
+        if region_width < Dungeon.MIN_WIDTH \
+            or region_height < Dungeon.MIN_HEIGHT:
+                error_str = "Region too small to generate room within: {} {}".\
+                    format(region_width, region_height)
+                raise ValueError()
 
-        # Generate characteristics of room
-        room_width = random.randint(cls.MIN_WIDTH,
-                        min(cls.MAX_WIDTH, region_width))
-        room_height = random.randint(cls.MIN_HEIGHT,
-                        min(cls.MAX_HEIGHT, region_height))
-        room_x = region_x + random.randint(0, region_width - room_width)
-        room_y = region_y + random.randint(0, region_height - room_height)
+        # Generate characteristics of the dungeon
+        if region_width == Dungeon.MIN_WIDTH:
+            room_width = cls.MIN_WIDTH
+            room_x = region_x + 1
+        else:
+            room_width = random.randint(cls.MIN_WIDTH,
+                            min(region_width - 2, cls.MAX_WIDTH))
+            room_x = random.randint(region_x + 1,
+                        (region_x + region_width - 1) - room_width)
+        if region_height == Dungeon.MIN_HEIGHT:
+            room_height = cls.MIN_HEIGHT
+            room_y = region_y + 1
+        else:
+            room_height = random.randint(cls.MIN_HEIGHT,
+                            min(region_height - 2, cls.MAX_HEIGHT))
+            room_y = random.randint(region_y + 1,
+                        (region_y + region_height - 1) - room_height)
         room = cls(room_x, room_y, room_width, room_height)
         print("room: ( {}, {}, {}, {} )".format(room_x, room_y,
                                                 room_width, room_height))
@@ -142,14 +157,16 @@ class Dungeon(pygame.sprite.Sprite):
     """ Represents the dungeon
 
         A dungeon is defined as a region in space consisting of a single room
-        surrounded by a void. Since the minimum room width and height is
-        defined to be 3, in order for a dungeon to be surrounded by void, the
-        minimum dungeon width and height must be 5. Rooms can only be placed
-        within the interior of the dungeon.
+        surrounded by a void. Since the absolute minimum room width and height
+        is defined to be 3, in order for a dungeon to be surrounded by void, the
+        absolute minimum dungeon width and height must be 5. However, the
+        actual minimum dimensions of the dungeon may be higher since they
+        depend on the actual minimum dimension of the room. Note that rooms can
+        only be placed within the interior of the dungeon.
 
         Each dungeon has an associated width, height, and surface to draw the
         dungeon upon. In addition, the dungeon stores the tiles at every
-        position within a tilemap. Lastly, a list of rooms is also stored
+        position within a tilemap. Lastly, a list of rooms is also stored.
     """
     def __init__(self, height=MAP_HEIGHT, width=MAP_WIDTH, tile_size=TILE_SIZE):
         """ Creates new dungeon object
@@ -203,9 +220,9 @@ class Dungeon(pygame.sprite.Sprite):
         return room
 
     MIN_WIDTH = Room.MIN_WIDTH + 2
-    MIN_HEIGHT = Room. MIN_HEIGHT + 2
+    MIN_HEIGHT = Room.MIN_HEIGHT + 2
     MAX_WIDTH = Room.MAX_WIDTH + 2
-    MAX_HEIGHT = Room. MAX_HEIGHT + 2
+    MAX_HEIGHT = Room.MAX_HEIGHT + 2
     @staticmethod
     def validate_region(region_width, region_height):
         """ Check the dimensions of a region """
@@ -227,38 +244,48 @@ class Dungeon(pygame.sprite.Sprite):
             Binary Space Partitioning as applied to game map generation will
             recursively divide the dungeon along a random dimension (left,
             right) each function call. Once the sub regions are of a desired
-            size, a room will be generated within the region. Note that the
-            regions are built in a top-down fashion. However, to connection
-            of the subregions must be done in a bottom-up fashion. Each sub
-            region in the dungeon must be connected to its sister. Once all the
-            regions have been connected, there is guaranteed to be a path from
-            every room to every other room and thus the dungeon is complete
-            (IS THIS TRUE????).
+            size, a room will be generated within the region. Since the room
+            within the region is surrounded by white space due to the way a
+            region is defined, no two rooms in the overall dungeon will
+            collide. Note that the regions are built in a top-down fashion.
+            However, to connection of the subregions must be done in a
+            bottom-up fashion. region in the dungeon must be connected to its
+            sister. Once all the regions have been connected, there is
+            guaranteed to be a path from every room to every other room and
+            thus the dungeon is complete.
 
         """
         print("Generating")
         print("region: ( {}, {}, {}, {} )".format(region_x, region_y,
                                                 region_width, region_height))
-        if Dungeon.validate_region(region_width, region_height) == 0:
+
+        split = True
+        if region_height < 2*Dungeon.MIN_HEIGHT:
+            """ If you subtract the minimum dungeon height from the top and
+                bottom of the region, you are left with a sort of bandwidth
+                regin where the split line can be placed such that each
+                subregion can hold a region of the minimum width such that a
+                room will be contained within the region and the room will be
+                surrounded by void.
+            """
+            print("can't split horizontally")
+            split = False
+        if region_width < 2*Dungeon.MIN_WIDTH:
+            # Similar logic as checking horiziontal split
+            print("can't split vertically")
+            split = False
+        if not split:
             self.add_rand_room(region_x, region_y,
-                                    region_width, region_height)
+                                region_width, region_height)
             return
 
         dung_split = random.choice(["horz", "vert"])
-        print(dung_split)
         if dung_split == "horz":
-            if region_height - Dungeon.MIN_HEIGHT <= Dungeon.MIN_HEIGHT:
-                # Can't split region anymore
-                print("cant split horizontally")
-                self.add_rand_room(region_x, region_y,
-                                        region_width, region_height)
-                return
-
             top_height = random.randint(Dungeon.MIN_HEIGHT,
-                                        region_height - Dungeon.MIN_HEIGHT)
+                            region_height - Dungeon.MIN_HEIGHT)
             top = (region_x, region_y, region_width, top_height)
-            bottom = (region_x, region_y + top_height - 1, region_width,
-                        region_height - top_height)
+            bottom = (region_x, region_y + top_height,
+                        region_width, region_height - top_height)
             if VISUALIZE:
                 start = (bottom[0] * TILE_SIZE, bottom[1] * TILE_SIZE)
                 end = ((bottom[0] + region_width) * TILE_SIZE, start[1])
@@ -266,16 +293,10 @@ class Dungeon(pygame.sprite.Sprite):
             self.generate_dungeon(*top)
             self.generate_dungeon(*bottom)
         else:
-            if region_width - Dungeon.MIN_WIDTH <= Dungeon.MIN_WIDTH:
-                # Can't split region anymore
-                print("cant split vertically")
-                self.add_rand_room(region_x, region_y,
-                                        region_width, region_height)
-                return
             left_width = random.randint(Dungeon.MIN_WIDTH,
-                                        region_width - Dungeon.MIN_WIDTH)
+                            region_width - Dungeon.MIN_WIDTH)
             left = (region_x, region_y, left_width, region_height)
-            right = (region_x + left_width - 1, region_y,
+            right = (region_x + left_width, region_y,
                         region_width - left_width, region_height)
             if VISUALIZE:
                 start = (right[0] * TILE_SIZE, right[1] * TILE_SIZE)
